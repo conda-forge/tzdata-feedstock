7b86c69a7fbbf62396349180a68d389771b1f449
H. Vetinari <h.vetinari@gmx.com>
2024-08-01 15:22:41 +1100

    also install leap-seconds.list

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+set -ex
+
+# by default the makefile does not install leap-seconds.list;
+# however, some implementations (e.g. libc++) rely on it so
+# we expand the default (TZDATA_TEXT=tzdata.zi leapseconds)
+
 make -e \
   DESTDIR=./build \
   USRDIR='' \
   POSIXRULES='' \
+  TZDATA_TEXT='tzdata.zi leapseconds leap-seconds.list' \
   install
 
 mkdir -p "${PREFIX}/share"
