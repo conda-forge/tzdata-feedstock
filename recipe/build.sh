a8859f81f79895aa3ff769931fbad44019e78e24
Claudius Haag <claudius.haag@web.de>
2025-11-30 21:50:06 +0100

    Fix for "calenders" being in both source-repos -> using the one from tzdata

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -ex
 
+# pull tzcode/* up one directory, except "calendars", as this already exists in tzdata
+if [[ -d tzcode ]]; then
+    # Move everything except "calendars"
+    find tzcode -mindepth 1 -maxdepth 1 \
+        -not -name calendars \
+        -exec mv -t . {} +
+fi
+
 # by default the makefile does not install leap-seconds.list;
 # however, some implementations (e.g. libc++) rely on it so
 # we expand the default (TZDATA_TEXT=tzdata.zi leapseconds)
